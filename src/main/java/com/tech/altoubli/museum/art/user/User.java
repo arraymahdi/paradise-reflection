2428a54e74157a4a02f892c48d905e82b931720c
mahdi <mahdiatubly@gmail.com>
2024-08-25 16:23:18 +0300

    Creating Post model

--- src/main/java/com/tech/altoubli/museum/art/user/User.java
+++ src/main/java/com/tech/altoubli/museum/art/user/User.java
@@ -1,12 +1,16 @@
 package com.tech.altoubli.museum.art.user;
 
+import com.tech.altoubli.museum.art.post.Post;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,6 +22,8 @@ public class User {
     private String firstName;
     private String lastName;
     private LocalDate birthDay;
+    @OneToMany
+    private List<Post> posts = new ArrayList<>();
 
     public String getFullName() {
         return getFirstName() + " " + getLastName();
