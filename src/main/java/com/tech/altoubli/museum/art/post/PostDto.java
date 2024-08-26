fef911e306a55a668a12b32d4b40627abf6776bd
mahdi <mahdiatubly@gmail.com>
2024-08-26 14:26:10 +0300

    Creating post DTO

---
+++ src/main/java/com/tech/altoubli/museum/art/post/PostDto.java
@@ -0,0 +1,18 @@
+package com.tech.altoubli.museum.art.post;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDto {
+    private Long userId;
+    private String imageUrl;
+    private String description;
+    private LocalDate createdAt;
+    private Boolean requireSubscription;
+}
