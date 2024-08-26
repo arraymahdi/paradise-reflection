4f8c67e276fe52a4b09d37b8b63eb5030f209579
mahdi <mahdiatubly@gmail.com>
2024-08-26 09:51:26 +0300

    Updating user model

--- src/main/java/com/tech/altoubli/museum/art/user/User.java
+++ src/main/java/com/tech/altoubli/museum/art/user/User.java
@@ -1,10 +1,7 @@
 package com.tech.altoubli.museum.art.user;
 
 import com.tech.altoubli.museum.art.post.Post;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,15 +12,49 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
+@Table(name = "users")
 public class User {
+
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String firstName;
     private String lastName;
     private LocalDate birthDay;
-    @OneToMany
+
+    @OneToMany(mappedBy = "creator", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Post> posts = new ArrayList<>();
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String phoneNumber;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_following",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "following_user_id")
+    )
+    private List<User> following = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "following")
+    private List<User> followers = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_subscribing",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "subscribing_user_id")
+    )
+    private List<User> subscribing = new ArrayList<>();
+
+    @ManyToMany(mappedBy = "subscribing")
+    private List<User> subscribers = new ArrayList<>();
 
     public String getFullName() {
         return getFirstName() + " " + getLastName();
