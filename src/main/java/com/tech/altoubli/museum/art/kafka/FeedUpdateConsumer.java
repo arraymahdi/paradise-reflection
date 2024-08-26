d8b306866f15f07346cbd08e2702f377d6968a5c
mahdi <mahdiatubly@gmail.com>
2024-08-26 14:46:12 +0300

    Cleaning FeedUpdateConsumer Class

--- src/main/java/com/tech/altoubli/museum/art/kafka/FeedUpdateConsumer.java
+++ src/main/java/com/tech/altoubli/museum/art/kafka/FeedUpdateConsumer.java
@@ -11,7 +11,6 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 @Service
