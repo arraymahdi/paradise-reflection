49d7f031510a3495b182bf67032557add28079f1
mahdi <mahdiatubly@gmail.com>
2024-08-26 13:53:34 +0300

    Creating PostNotFoundException

---
+++ src/main/java/com/tech/altoubli/museum/art/exception/PostNotFoundExcdeption.java
@@ -0,0 +1,7 @@
+package com.tech.altoubli.museum.art.exception;
+
+public class PostNotFoundExcdeption extends RuntimeException {
+    public PostNotFoundExcdeption(String message){
+        super(message);
+    }
+}
