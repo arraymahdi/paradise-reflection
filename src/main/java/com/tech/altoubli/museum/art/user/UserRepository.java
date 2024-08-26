99ade44b6ac10dc40bc5fb957c0851a665815047
mahdi <mahdiatubly@gmail.com>
2024-08-26 16:15:40 +0300

    Updating user repository

--- src/main/java/com/tech/altoubli/museum/art/user/UserRepository.java
+++ src/main/java/com/tech/altoubli/museum/art/user/UserRepository.java
@@ -2,5 +2,10 @@ package com.tech.altoubli.museum.art.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    List<User> findByRole_Name(String roleName);
 }
