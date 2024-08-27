75a29c257b125434c958dc5cf231555b9e1e2b8a
mahdi <mahdiatubly@gmail.com>
2024-08-27 15:45:14 +0300

    Cleaning admin controller

--- src/main/java/com/tech/altoubli/museum/art/admin/AdminController.java
+++ src/main/java/com/tech/altoubli/museum/art/admin/AdminController.java
@@ -1,7 +1,6 @@
 package com.tech.altoubli.museum.art.admin;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
