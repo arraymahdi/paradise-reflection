b0a1e0a893a47b81b3b6e17d49c44fb9e3ab647b
mahdi <mahdiatubly@gmail.com>
2024-08-27 15:54:21 +0300

    Initializing the role table using CommandLineRunner Bean

--- src/main/java/com/tech/altoubli/museum/art/Application.java
+++ src/main/java/com/tech/altoubli/museum/art/Application.java
@@ -1,7 +1,11 @@
 package com.tech.altoubli.museum.art;
 
+import com.tech.altoubli.museum.art.role.Role;
+import com.tech.altoubli.museum.art.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +14,17 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository) {
+		return args -> {
+			if (roleRepository.findByName("ROLE_USER").isEmpty()) {
+				roleRepository.save(Role.builder().name("ROLE_USER").build());
+			}
+			if (roleRepository.findByName("ROLE_ADMIN").isEmpty()) {
+				roleRepository.save(Role.builder().name("ROLE_ADMIN").build());
+
+			}
+
+		};
+	}
 }
