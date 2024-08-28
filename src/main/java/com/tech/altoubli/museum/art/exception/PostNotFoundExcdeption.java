6ddde69e0ecbdc953b5f8cc558d36c39c94cb33f
mahdi <mahdiatubly@gmail.com>
2024-08-28 09:34:52 +0300

    Fixing a typo in the class name

--- src/main/java/com/tech/altoubli/museum/art/exception/PostNotFoundExcdeption.java
+++
@@ -1,7 +0,0 @@
-package com.tech.altoubli.museum.art.exception;
-
-public class PostNotFoundExcdeption extends RuntimeException {
-    public PostNotFoundExcdeption(String message){
-        super(message);
-    }
-}
