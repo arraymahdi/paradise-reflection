31e0761244c109173026e87fc01935e03f8f9147
mahdi <mahdiatubly@gmail.com>
2024-08-28 19:41:05 +0300

    Fixing delete cascading issue in the feed model

--- src/main/java/com/tech/altoubli/museum/art/feed/Feed.java
+++ src/main/java/com/tech/altoubli/museum/art/feed/Feed.java
@@ -19,7 +19,7 @@ public class Feed {
     private Long id;
     @OneToOne
     private User user;
-    @ManyToMany
+    @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "feed_posts",
             joinColumns = @JoinColumn(name = "feed_id"),
