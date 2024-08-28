1394dabbf7b60aa4dcfa080102d19c035fee61c1
mahdi <mahdiatubly@gmail.com>
2024-08-28 15:41:35 +0300

    Adding email service to send email notifications with following requests

--- src/main/java/com/tech/altoubli/museum/art/email/EmailService.java
+++ src/main/java/com/tech/altoubli/museum/art/email/EmailService.java
@@ -1,5 +1,6 @@
 package com.tech.altoubli.museum.art.email;
 
+import com.tech.altoubli.museum.art.following_request.FollowingRequest;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,6 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -25,7 +25,7 @@ public class EmailService {
     private final SpringTemplateEngine templateEngine;
 
     @Async
-    public void sendEmail(
+    public void sendAccountActivationEmail(
             String to,
             String username,
             EmailTemplateName emailTemplate,
@@ -33,32 +33,60 @@ public class EmailService {
             String activationCode,
             String subject
     ) throws MessagingException {
-        String templateName;
-        if (emailTemplate == null) {
-            templateName = "confirm-email";
-        } else {
-            templateName = emailTemplate.name();
-        }
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("confirmationUrl", confirmationUrl);
+        properties.put("activation_code", activationCode);
+
+        sendEmail(to, username, emailTemplate, "activate-account", subject, properties);
+    }
+
+    @Async
+    public void sendFollowingRequestEmail(
+            String to,
+            String username,
+            EmailTemplateName emailTemplate,
+            String confirmationUrl,
+            String declineUrl,
+            String subject,
+            FollowingRequest followingRequest
+    ) throws MessagingException {
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("confirmationUrl", confirmationUrl);
+        properties.put("declineUrl", declineUrl);
+        properties.put("followingRequest", followingRequest);
+
+        sendEmail(to, username, emailTemplate, "following-request", subject, properties);
+    }
+
+    private void sendEmail(
+            String to,
+            String username,
+            EmailTemplateName emailTemplate,
+            String defaultTemplate,
+            String subject,
+            Map<String, Object> additionalProperties
+    ) throws MessagingException {
+        String templateName = (emailTemplate != null) ? emailTemplate.name() : defaultTemplate;
+
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper helper = new MimeMessageHelper(
                 mimeMessage,
                 MULTIPART_MODE_MIXED,
                 UTF_8.name()
         );
-        Map<String, Object> properties = new HashMap<>();
+
+        Map<String, Object> properties = new HashMap<>(additionalProperties);
+        properties.put("to", to);
         properties.put("username", username);
-        properties.put("confirmationUrl", confirmationUrl);
-        properties.put("activation_code", activationCode);
 
         Context context = new Context();
         context.setVariables(properties);
 
-        helper.setFrom("CodeExpert@Altoubli.com");
+        helper.setFrom("notification@museum.art.com");
         helper.setTo(to);
         helper.setSubject(subject);
 
         String template = templateEngine.process(templateName, context);
-
         helper.setText(template, true);
 
         mailSender.send(mimeMessage);
