575c10cb0ff260b531270d1858d07309916c5c48
mahdi <mahdiatubly@gmail.com>
2024-08-28 06:16:30 +0300

    Updating Post Controller

--- src/main/java/com/tech/altoubli/museum/art/post/PostController.java
+++ src/main/java/com/tech/altoubli/museum/art/post/PostController.java
@@ -1,8 +1,14 @@
 package com.tech.altoubli.museum.art.post;
 
+import com.tech.altoubli.museum.art.user.User;
+import com.tech.altoubli.museum.art.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -10,14 +16,23 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping
-    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
-        return ResponseEntity.ok(postService.createPost(postDto));
+    @PostMapping("/create")
+    public ResponseEntity<PostDto> createPost(@RequestPart("description") String description,
+                                              @RequestPart("requireSubscription") String requireSubscription,
+                                              @RequestPart("file") MultipartFile file,
+                                              Authentication connectedUser) {
+        PostDto postDto = postService.createPost(description, Boolean.valueOf(requireSubscription), file, connectedUser);
+        return new ResponseEntity<>(postDto, HttpStatus.CREATED);
     }
 
     @GetMapping("/{postId}")
-    public ResponseEntity<PostDto> getPostById(@PathVariable Long postId) {
-        return ResponseEntity.ok(postService.getPostById(postId));
+    public ResponseEntity<PostDto> getPostById(@PathVariable Long postId,
+                                               Authentication connectedUser) {
+        User user = userRepository.findByEmail(connectedUser.getName())
+                .orElseThrow(()-> new UsernameNotFoundException("User Not Found"));
+        return ResponseEntity.ok(postService.getPostById(postId, user));
     }
 }
