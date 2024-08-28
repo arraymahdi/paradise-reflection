79348a9e5271fc92417930657748be5809180107
mahdi <mahdiatubly@gmail.com>
2024-08-28 07:06:40 +0300

    Adding get user feed service

--- src/main/java/com/tech/altoubli/museum/art/user/UserService.java
+++ src/main/java/com/tech/altoubli/museum/art/user/UserService.java
@@ -1,10 +1,14 @@
 package com.tech.altoubli.museum.art.user;
 
 import com.tech.altoubli.museum.art.exception.UsernameNotUniqueException;
+import com.tech.altoubli.museum.art.post.PostDto;
+import com.tech.altoubli.museum.art.user_profile.UserProfileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -12,6 +16,7 @@ public class UserService {
 
 
     private final UserRepository userRepository;
+    private final UserProfileService userProfileService;
 
 
     public Boolean changeAccountStatus(Boolean booleanStatus, User user) {
@@ -28,5 +33,17 @@ public class UserService {
         user.setUsername(username);
         userRepository.save(user);
         return username;
+    }
+
+    public List<PostDto> getUserFeed(User user) {
+        return user.getFeed().getPosts().stream()
+                .map((post)-> PostDto
+                        .builder()
+                        .userProfile(userProfileService.getProfile(post.getCreator()).getBody())
+                        .description(post.getDescription())
+                        .imageUrl(post.getImageUrl())
+                        .requireSubscription(post.getRequireSubscription())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
