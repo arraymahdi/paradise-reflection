94234a4e71da990741a84ba3b14e07a127db41fc
mahdi <mahdiatubly@gmail.com>
2024-08-28 15:42:48 +0300

    Adding following request email template to email templates

--- src/main/java/com/tech/altoubli/museum/art/email/EmailTemplateName.java
+++ src/main/java/com/tech/altoubli/museum/art/email/EmailTemplateName.java
@@ -7,9 +7,7 @@ public enum EmailTemplateName {
 
     ACTIVATE_ACCOUNT("activate_account"),
     RESET_FORGOTTEN_PASSWORD("reset_forgotten_password"),
-    JOINING_INVITATION("joining_invitation"),
-    JOINING_REQUEST("joining_request")
-    ;
+    FOLLOWING_REQUEST("following_request");
 
 
     private final String name;
