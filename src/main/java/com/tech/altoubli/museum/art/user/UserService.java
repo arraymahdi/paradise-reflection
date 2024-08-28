c06f3cebba1fa14a16dd362a2c7a7a1d96291483
mahdi <mahdiatubly@gmail.com>
2024-08-28 06:51:02 +0300

    Adding change username service

--- src/main/java/com/tech/altoubli/museum/art/user/UserService.java
+++ src/main/java/com/tech/altoubli/museum/art/user/UserService.java
@@ -1,7 +1,10 @@
 package com.tech.altoubli.museum.art.user;
 
+import com.tech.altoubli.museum.art.exception.UsernameNotUniqueException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,15 @@ public class UserService {
         user.setIsPublic(booleanStatus);
         userRepository.save(user);
         return booleanStatus;
+    }
+
+    public String changeUserName(User user, String username) {
+        Optional<User> checkUsername = userRepository.findByUsername(username);
+        if(checkUsername.isPresent()){
+            throw new UsernameNotUniqueException("This username has been taken");
+        }
+        user.setUsername(username);
+        userRepository.save(user);
+        return username;
     }
 }
